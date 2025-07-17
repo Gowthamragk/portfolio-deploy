535e42d5b7a13a071c2d2e67644d407cab5f9770
Gowthamragk <136979852+Gowthamragk@users.noreply.github.com>
2025-07-17 23:26:17 +0530

    Modify python while code

--- while_loop_python.py
+++ while_loop_python.py
@@ -9,3 +9,12 @@ num = 1+2j
 
 num_sqrt = cmath.sqrt(num)
 print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
